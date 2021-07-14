9fcaf7d734fe35ae6c431e1c057ef9dd233684a0
Jared Jones <jared@jaredcj.com>
2021-07-13 19:41:26 -0600

    Tidy.

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,44 @@
 import "./style.css";
 import * as THREE from "three";
 
-// Query the DOM.
-const app = document.querySelector<HTMLDivElement>("#app")!;
+// ===== QUERY DOM
 const canvas = document.querySelector<HTMLCanvasElement>("#three-canvas")!;
 const canvasContainer = document.querySelector<HTMLDivElement>("#three-container")!;
 
-// Get values.
+// ===== SETUP constants
 const containerWidth = canvasContainer.offsetWidth;
 const containerHeight = canvasContainer.offsetHeight;
 const containerRatio = containerWidth / containerHeight;
 const pixelRatio = window.devicePixelRatio;
 
-// Set up three.js.
+// ===== SETUP three.js
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
   canvas,
   antialias: true,
 });
-renderer.setSize( containerWidth, containerHeight );
+renderer.setSize(containerWidth, containerHeight);
 renderer.setPixelRatio(pixelRatio);
 
-const camera = new THREE.PerspectiveCamera( 75, containerRatio , 0.1, 1000 );
+// ===== CAMERA
+const camera = new THREE.PerspectiveCamera(75, containerRatio, 0.1, 1000);
 camera.position.z = 6;
 
-const geometry = new THREE.BoxGeometry(2, 2, 2, 3, 3, 3);
+// ===== GROUP
+const mainGroup = new THREE.Group();
+scene.add(mainGroup);
 
-const material = new THREE.MeshBasicMaterial({
+// ===== CUBE
+const cubeGeometry = new THREE.BoxGeometry(2, 2, 2, 3, 3, 3);
+const cubeMaterial = new THREE.MeshBasicMaterial({
   color: 0xff0000,
 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+mainGroup.add(cube);
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-// Render/animate three.js.
+// ===== Render/animate three.js.
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 animate();
